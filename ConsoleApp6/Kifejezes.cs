3384e31d34e7d630ed7b40672c903d5e6b8bb007
asd <asd@DESKTOP-ULI68NB>
2023-05-24 11:11:46 +0200

    Add project files.

---
+++ ConsoleApp6/Kifejezes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Kifejezes
+    {
+        int elsoOperandus;
+        string karakter;
+        int masodikOperandus;
+
+        public Kifejezes(int elsoOperandus, string karakter, int masodikOperandus)
+        {
+            this.elsoOperandus = elsoOperandus;
+            this.karakter = karakter;
+            this.masodikOperandus = masodikOperandus;
+        }
+
+        public int ElsoOperandus { get => elsoOperandus; set => elsoOperandus = value; }
+        public string Karakter { get => karakter; set => karakter = value; }
+        public int MasodikOperandus { get => masodikOperandus; set => masodikOperandus = value; }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp6;
+
+internal class Program
+{
+    static void Main()
+    {
+        List<Kifejezes> kifejezesek = new List<Kifejezes>();
+
+        StreamReader sr = new StreamReader("kifejezesek.txt");
+        sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(" ");
+
+            Kifejezes uj = new Kifejezes(
+            int.Parse(mezok[0]),
+            mezok[1],
+            int.Parse(mezok[2]));
+            kifejezesek.Add(uj);
+            }
+        sr.Close();
+
+        Console.WriteLine(kifejezesek.Count);
+    }
+}
